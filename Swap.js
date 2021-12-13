7f53bb31f271b13cf86e4b57a99a820f5697a5f0
R-동기화 <83122757+22388o@users.noreply.github.com>
2021-12-13 13:00:13 -0300

    Create Swap.js

---
+++ Swap.js
@@ -0,0 +1,81 @@
+// lib/swap.js
+
+class Swap {
+  constructor(lib, network){
+    // Require the library by name and set the network for every module
+    this.libName = lib;
+    this.lib = require(lib);
+    this.lib.Network.set(network);
+
+    // Import each module we'll need from the library
+    this.Outpoint = this.lib.Outpoint;
+    this.Coin = this.lib.Coin;
+    this.MTX = this.lib.MTX;
+    this.TX = this.lib.TX;
+    this.Address = this.lib.Address;
+    this.hd = this.lib.hd;
+    this.KeyRing = this.lib.KeyRing;
+    this.Script = this.lib.Script;
+    this.Stack = this.lib.Stack;
+    this.consensus = this.lib.consensus;
+    this.util = this.lib.util;
+    this.ChainEntry = this.lib.ChainEntry;
+
+    // Verify things like SegWit, CSV, Bitcoin Cash FORKID, etc.
+    this.flags = this.Script.flags.STANDARD_VERIFY_FLAGS;
+
+    // We will base our relative locktime on TIME, not BLOCKS
+    this.CSV_seconds = true;
+  }
+  // lib/swap.js
+
+class Swap {
+  ...
+
+  // REDEEM script: the output of the swap HTLC
+  getRedeemScript(hash, refundPubkey, swapPubkey, locktime){
+    const redeem = new this.Script();
+
+    redeem.pushSym('OP_IF');
+    redeem.pushSym('OP_SHA256');
+    redeem.pushData(hash);
+    redeem.pushSym('OP_EQUALVERIFY');
+    redeem.pushData(swapPubkey);
+    redeem.pushSym('OP_CHECKSIG');
+    redeem.pushSym('OP_ELSE');
+    redeem.pushInt(locktime);
+    redeem.pushSym('OP_CHECKSEQUENCEVERIFY');
+    redeem.pushSym('OP_DROP');
+    redeem.pushData(refundPubkey);
+    redeem.pushSym('OP_CHECKSIG');
+    redeem.pushSym('OP_ENDIF');
+    redeem.compile();
+
+    return redeem;
+  }
+
+  // SWAP script: used by counterparty to open the hash lock 
+  getSwapInputScript(redeemScript, secret){
+    const inputSwap = new this.Script();
+
+    inputSwap.pushInt(0); // signature placeholder
+    inputSwap.pushData(secret);
+    inputSwap.pushInt(1); // <true>
+    inputSwap.pushData(redeemScript.toRaw()); // P2SH
+    inputSwap.compile();
+
+    return inputSwap;
+  }
+
+  // REFUND script: used by original sender of funds to open time lock
+  getRefundInputScript(redeemScript){
+    const inputRefund = new this.Script();
+
+    inputRefund.pushInt(0); // signature placeholder
+    inputRefund.pushInt(0); // <false>
+    inputRefund.pushData(redeemScript.toRaw()); // P2SH
+    inputRefund.compile();
+
+    return inputRefund;
+  }
+}
